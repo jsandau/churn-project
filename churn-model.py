f6225f236ed277a76c8b8e127760c236119df48f
Jsandau <jsandau@sandau.com>
2026-04-17 17:12:26 -0500

    test commit

--- churn-model.py
+++ churn-model.py
@@ -1,1 +1,9 @@
 ## Define churn  -> churn = churn_flag in subscriptions 
+# Github actions:
+# git status
+# git add .
+# git commit -m "churn model"
+# git push origin main
+
+import pandas as pd
+import numpy as np
